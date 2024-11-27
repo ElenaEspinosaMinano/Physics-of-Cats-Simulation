99b6c7161cde9702a636a1e8b446cad7b1144a2f
Elena Espinosa Miñano <elenaespinosami@gmail.com>
2024-11-27 17:26:13 +0000

    Mini update

--- Model_1/elena_model_1_v2.py
+++ Model_1/elena_model_1_v2.py
@@ -65,7 +65,7 @@ z = R * np.cos(theta)
 
 ### Plot the results
 plt.figure()
-#plt.plot(t_array, theta, label='theta (angle)')
+plt.plot(t_array, theta, label='theta (angle)')
 #plt.plot(t_array, theta_dot, label='theta_dot (angular velocity)')
 plt.plot(t_array, z, label='z (height)')
 plt.xlabel('Time, t (in seconds)')
